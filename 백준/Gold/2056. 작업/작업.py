dd63ebf3e362f858912d414c31337ce0b1df98b2
김태한 <101809854+Kimtaehan2@users.noreply.github.com>
2023-01-02 21:57:09 +0900

    [Gold IV] Title: 작업, Time: 364 ms, Memory: 37900 KB -BaekjoonHub

---
+++ 백준/Gold/2056. 작업/작업.py
@@ -0,0 +1,45 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n = int(input())
+
+graph = [[] for _ in range(n)]
+work_time = [0]*n
+in_degree = [0]*n
+visited = [0]*n
+time = [0]*n
+maxtime = [0]*n
+
+start_indegree = []
+
+for i in range(n):
+    cmd = list(map(int,input().split()))
+    work_time[i] = cmd[0]
+    if cmd[1] > 0:
+        for x in cmd[2:]:
+            graph[x-1].append(i)
+            in_degree[i] += 1
+    else:
+        time[i] = cmd[0]
+        maxtime[i] = cmd[0]
+        start_indegree.append(i)
+
+for i in start_indegree:
+    visited[i] = 1
+
+def bfs():
+    que = deque(start_indegree)
+    while que:
+        new = deque.popleft(que)
+        for x in graph[new]:
+            in_degree[x] -= 1
+            if maxtime[x] < time[new]:
+                maxtime[x] = time[new]
+            
+            if in_degree[x] == 0:
+                time[x] += (maxtime[x]+work_time[x])
+                que.append(x)
+
+bfs()
+print(max(time))
